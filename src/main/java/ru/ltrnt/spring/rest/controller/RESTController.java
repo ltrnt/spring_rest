dbd55d9dc4e8604fd3a6cd9b1d24ac229115f1cd
Maxim Filippov <maxim.fil1905@yandex.ru>
2021-02-10 00:59:06 +0400

    Add api/employees/{id} request

--- src/main/java/ru/ltrnt/spring/rest/controller/RESTController.java
+++ src/main/java/ru/ltrnt/spring/rest/controller/RESTController.java
@@ -2,6 +2,7 @@ package ru.ltrnt.spring.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.ltrnt.spring.rest.entity.Employee;
@@ -19,6 +20,11 @@ public class RESTController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable int id) {
+        return employeeService.getEmployee(id);
     }
 
 }
